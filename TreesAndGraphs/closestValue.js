69aad992bff07c10429ca89c2ef39657214b64b4
IvanVazquezL <ivanvl_11@hotmail.com>
2024-09-18 23:20:26 -0600

    (feat): searchBST | BST | EASY

---
+++ TreesAndGraphs/closestValue.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} target
+ * @return {number}
+ */
+var closestValue = function(root, target) {
+    let val = root.val;
+    let closest = root.val;
+
+    while (root != null) {
+        val = root.val;
+        closest = Math.abs(val - target) < Math.abs(closest - target)
+            || (Math.abs(val - target) === Math.abs(closest - target) && val < closest) ?
+            val : 
+            closest;
+        root = target < root.val ? root.left : root.right;
+    }
+
+    return closest;
+};
