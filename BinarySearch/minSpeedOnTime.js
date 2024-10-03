49a2607fca00a00669c8d008493157c5444bc782
IvanVazquezL <ivanvl_11@hotmail.com>
2024-10-02 23:25:01 -0600

    (feat): minSpeedOnTime | medium | binarySearch

---
+++ BinarySearch/minSpeedOnTime.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number[]} dist
+ * @param {number} hour
+ * @return {number}
+ */
+var minSpeedOnTime = function(dist, hour) {
+    if (dist.length > Math.ceil(hour)) {
+        return -1;
+    }
+
+    let left = 1;
+    let right = Math.pow(10,7);
+
+    while (left <= right) {
+        const mid = Math.floor((left + right)/2);
+
+        if (check(mid)) {
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+
+    return left;
+
+    function check(k) {
+        let time = 0;
+
+        for (const distance of dist) {
+            time = Math.ceil(time);
+            time += distance/k;
+        }
+
+        return time <= hour;
+    }
+};
